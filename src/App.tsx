06c37593039a10d2914c59b8f9081555ca3a1729
WitchLosser <anton.kozak230@gmail.com>
2023-06-30 01:21:40 +0300

    Create admin panel, crud for categories, main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CategoryListPage from "./compnents/admin/category/list/CategoryListPage";
+import CategoryCreatePage from "./compnents/admin/category/create/CategoryCreatePage";
+import CategoryEditPage from "./compnents/admin/category/edit/CategoryEditPage";
+import AdminLayout from "./compnents/admin/container/AdminLayout";
+import AdminDashboard from "./compnents/admin/dashboard/AdminDashboard";
+import HomePage from "./compnents/home/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<HomePage />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="category">
+            <Route index element={<CategoryListPage />} />
+            <Route path="create" element={<CategoryCreatePage />} />
+            <Route path="edit">
+              <Route path=":id" element={<CategoryEditPage />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
